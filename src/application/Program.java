0b2655b38a82ab20e535683ffed55f3981d5d559
Virgilio Sirqueira Cruvinel <virgiliocomp@gmail.com>
2025-06-23 20:34:21 -0300

    Desafio finalizado

---
+++ src/application/Program.java
@@ -0,0 +1,59 @@
+package application;
+
+import entities.Sale;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Entre o caminho do arquivo: ");
+        String path = sc.nextLine();
+
+        try {
+
+            List<Sale> sales = new ArrayList<>();
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String line;
+            while ((line = br.readLine()) != null){
+                String[] lineContent = line.split(",");
+                int month = Integer.parseInt(lineContent[0]);
+                int year = Integer.parseInt(lineContent[1]);
+                String seller = lineContent[2];
+                int items = Integer.parseInt(lineContent[3]);
+                double total = Double.parseDouble(lineContent[4]);
+
+                sales.add(new Sale(month,year, seller, items, total));
+            }
+
+            List<Sale> fiveFirstSalesWithHighestAverage = sales.stream()
+                    .filter(x -> x.getYear() == 2016)
+                    .sorted(Comparator.comparingDouble(Sale::averagePrice).reversed())
+                    .limit(5)
+                    .toList();
+
+            fiveFirstSalesWithHighestAverage.forEach(System.out::println);
+
+            double loganTotalSales = sales.stream()
+                    .filter(x -> x.getSeller().equals("Logan"))
+                    .filter(x -> x.getMonth() == 1 || x.getMonth() == 7)
+                    .mapToDouble(Sale::getTotal)
+                    .sum();
+
+            System.out.println("\nValor total vendido pelo vendedor Logan nos meses 1 e 7 = " + loganTotalSales);
+
+        }
+        catch (IOException e){
+            System.out.println("Erro: " + path + " (O sistema não pode encontrar o arquivo especificado");
+        }
+
+
+    }
+}
